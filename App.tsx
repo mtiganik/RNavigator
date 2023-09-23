ff4980a1cab0c842152205328a0214ada6e1a9ef
Mihkel Tiganik <mtiganik@gmail.com>
2023-09-23 10:38:52 +0300

    React Native Navigator examle

--- App.tsx
+++ App.tsx
@@ -1,14 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, useWindowDimensions } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StackScreenProps } from '@react-navigation/stack';
+const Stack = createNativeStackNavigator<StackParamList>();
+
+type PropsHome = StackScreenProps<StackParamList, 'Home'>;
+const HomeScreen = ({ route, navigation }: PropsHome) => {
+  const window = useWindowDimensions();
+    return (
+      <View style={styles.container}>
+      <Button
+          title="Go to Details"
+          onPress={() => navigation.navigate('Details', { userId: 'akaver' })}
+      />
+      <Text>Phone size: {window.width}x{window.height}</Text>
+      <Text></Text>
+  </View>
+    );
+}
+type PropsDetails = StackScreenProps<StackParamList, 'Details'>;
+const DetailsScreen = ({ route, navigation }: PropsDetails) => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Details Screen {route.params.userId}</Text>
+        </View>
+    );
+}
+
+export type StackParamList = {
+  Home: undefined;
+  Details: { userId: string };
+};
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={HomeScreen} options={{ title: 'Home Screen' }}/>
+        <Stack.Screen name='Details' component={DetailsScreen} options={{ title: 'Details Screen' }}  initialParams={{ userId: 'def value' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
